1474ec976ad96c40512b5459c69d56e649489f9b
Fahd Sahlane <sahlane.fahd@etu.uae.ac.ma>
2026-01-22 06:23:15 +0100

    Revert "Merge pull request #2 from fahdlevreeey/youssef-branch"
    This reverts commit fc9340ae4f900c1cad1ad9c3d9a30981f2f7cb0c, reversing
    changes made to b5a39e61cab6494ab880d28158ff0e655cda31aa.

--- Cenoria/settings.py
+++ Cenoria/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -41,7 +40,6 @@ INSTALLED_APPS = [
     "playlists",
     "interactions",
     "movies",
-    "accounts.apps.AccountsConfig"
 ]
 
 MIDDLEWARE = [
@@ -59,7 +57,7 @@ ROOT_URLCONF = "Cenoria.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
