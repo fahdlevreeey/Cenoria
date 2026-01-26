24f95f0bfddaf82e27d1d17ff180dbca000c6f7a
Fahd Sahlane <sahlane.fahd@etu.uae.ac.ma>
2026-01-26 16:29:10 +0100

    Integration de l'app accounts avec templates et reglage dans Settings et Urls

--- Cenoria/settings.py
+++ Cenoria/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "playlists",
     "interactions",
     "movies",
+    "accounts.apps.AccountsConfig"
 ]
 
 MIDDLEWARE = [
--- Cenoria/urls.py
+++ Cenoria/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("playlists/", include("playlists.urls")),
+    path("", include("accounts.urls"))
 ]
